cfb3adf427524d102feb74cbbbf1a268c217a71e
Elledgef <elledgef@oregonstate.edu>
2022-11-05 17:21:31 -0700

    update after office hours help

--- is_subsequence.py
+++ is_subsequence.py
@@ -5,14 +5,15 @@
 
 
 def is_subsequence(string_a, string_b):
-    if string_a == 0:
+
+    if string_a == "":
         return True
-    if string_b == 0:
+    if string_b == "":
         return False
-    if string_a[0] == string_b[0]:
-        return is_subsequence(string_a[:-1], string_b[:-1])
+    if string_a[1] == string_b[1]:
+        return is_subsequence(string_a[1:], string_b[1:])
     else:
-        return is_subsequence(string_a, string_a[:-1])
+        return is_subsequence(string_a, string_b[1:])
 
 
 
